185f8df2af76ae4c3286e3f4423fc4d7fbb3c145
gayatri-v <gayatrishende1205@gmail.com>
2024-02-10 21:46:29 +0530

    second commit

--- Server/index.js
+++ Server/index.js
@@ -6,13 +6,6 @@ const PORT = 9000;
 app.get("/", (req, res) => {
   res.send("API is running..");
 });
-
-app.get("/about", (req, res) => {
-  res.send("About us..");
-});
-
-
-
 app.listen(PORT, () => {
   console.log(`API is running on http://localhost:${PORT}`);
 });
