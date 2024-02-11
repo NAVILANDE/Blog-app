b83065e4ff6a6d45dd338803ffa5c5774d7693c1
gayatri-v <gayatrishende1205@gmail.com>
2024-02-11 11:51:00 +0530

    mongodb

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+//const mongoose=require("mongoose");
 const connectToMongo=async()=>{
-    const res=await mongoose.connect("mongodb://localhost:27017/blog-mern-project");
+    const res=await mongoose.connect("mongodb://localhost:27017");
     if(res)
     {
         console.log("connected successfully");
