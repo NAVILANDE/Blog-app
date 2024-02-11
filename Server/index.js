67f7f9ea85d0fcf388f6525ffb97a778dbfe68f1
gayatri-v <gayatrishende1205@gmail.com>
2024-02-11 12:08:23 +0530

    fourth commit

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,8 @@
 import express from "express";
-
+import connectToMongo from "./config/db.js";
 const app = express();
 const PORT = 9000;
-
+connectToMongo();
 app.get("/", (req, res) => {
   res.send("API is running..");
 });
